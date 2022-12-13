86352d9f063c8a9eb3c00924e74c8a4b9de623dd
radum <radumuresan625@gmail.com>
2022-12-13 19:52:36 +0200

    Setters & Getters

--- src/NewStuff.java
+++ src/NewStuff.java
@@ -2,6 +2,39 @@ public class NewStuff {
     int j;
     int k;
     int v;
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public int getGgg() {
+        return ggg;
+    }
+
+    public void setGgg(int ggg) {
+        this.ggg = ggg;
+    }
+
     int ggg;
 
 }
