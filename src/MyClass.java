a8ae2e1ff579998010538b73ea093aff8cf7c89b
radum <radumuresan625@gmail.com>
2022-12-13 19:01:56 +0200

    Prima Schimbare

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,6 +2,7 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("funct 2 ");
+        System.out.println("Test 123" + " test 456");
 
     }
 }
